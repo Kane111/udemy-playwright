579756da97e2b96ca7e6d907dc2e99a0eafbfd76
Ickerion <139379191+Ickerion@users.noreply.github.com>
2026-09-21 15:54:45 +0300

    Custom Helpers (Functions)

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,5 +1,7 @@
 import { test, expect } from '@playwright/test'
 
+import { loadHomepage, assertTitle } from '../helpers'
+
 test("Simple basic test", async ({ page }) => {
   await page.goto("https://example.com/")
   const pageTitle = await page.locator('h1')
@@ -68,7 +70,7 @@ test("Assertions @myTag", async ({ page }) => {
 })
 })
 
-test.describe.only("Hooks", () => {
+test.describe("Hooks", () => {
   test.beforeEach(async ({ page }) => {
     await page.goto('https://example.com/')
   })
@@ -91,5 +93,9 @@ test("Single Element Screenshot", async ({ page }) => {
   const element = await page.$('h1')
   await element.screenshot({ path: "single_element_screenshot.png" })
 })
-
 })
+
+test.only ("Custom Helpers", async ({ page }) => {
+  await loadHomepage(page)
+  await assertTitle(page)
+} )
